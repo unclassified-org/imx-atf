b908f268fd200623f0e56066dfdc2f54b66d04a7
Sandrine Bailleux <sandrine.bailleux@arm.com>
2017-04-19 16:59:04 +0100

    WIP: Changes to architectural defs
    Change-Id: I2759bf30dec9c74f190545b63cab51e6e39bdd80

--- include/lib/xlat_tables/xlat_tables_defs.h
+++ include/lib/xlat_tables/xlat_tables_defs.h
@@ -22,6 +22,17 @@
 #define TWO_MB_INDEX(x)		((x) >> TWO_MB_SHIFT)
 #define FOUR_KB_INDEX(x)	((x) >> FOUR_KB_SHIFT)
 
+/*
+ * Terminology:
+ *
+ * - A block descriptor points to a region of memory bigger than the granule
+ *   size (e.g. a 2MB region when the granule size is 4KB).
+ *
+ * - A page descriptor points to a page, i.e. a memory region whose size is
+ *   the translation granule size (e.g. 4KB).
+ *
+ * - A table descriptor points to the next level of translation table.
+ */
 #define INVALID_DESC		0x0
 #define BLOCK_DESC		0x1 /* Table levels 0-2 */
 #define TABLE_DESC		0x3 /* Table levels 0-2 */
@@ -32,21 +43,29 @@
 #define SECOND_LEVEL_DESC_N	TWO_MB_SHIFT
 #define THIRD_LEVEL_DESC_N	FOUR_KB_SHIFT
 
+#define XN_SHIFT		54
+
+/*
+ * The following definitions must all be passed to the UPPER_ATTRS() macro to
+ * get the right bitmask.
+ */
 /* XN: Translation regimes that support one VA range (EL2 and EL3). */
 #define XN			(ULL(1) << 2)
 /* UXN, PXN: Translation regimes that support two VA ranges (EL1&0). */
 #define UXN			(ULL(1) << 2)
 #define PXN			(ULL(1) << 1)
 #define CONT_HINT		(ULL(1) << 0)
-#define UPPER_ATTRS(x)		(((x) & ULL(0x7)) << 52)
-
 #define NON_GLOBAL		(1 << 9)
 #define ACCESS_FLAG		(1 << 8)
 #define NSH			(0x0 << 6)
 #define OSH			(0x2 << 6)
 #define ISH			(0x3 << 6)
+#define UPPER_ATTRS(x)		(((x) & ULL(0x7)) << 52)
 
 #define TABLE_ADDR_MASK		ULL(0x0000FFFFFFFFF000)
+
+/* XXX: Check redundancy with TABLE_ADDR_MASK */
+#define XLAT_TABLE_IDX_MASK	0x1ff
 
 #define PAGE_SIZE_SHIFT		FOUR_KB_SHIFT /* 4, 16 or 64 KB */
 #define PAGE_SIZE		(1 << PAGE_SIZE_SHIFT)
@@ -87,17 +106,34 @@
 #define XLAT_ADDR_MASK(level)	(~XLAT_BLOCK_MASK(level))
 
 /*
- * AP[1] bit is ignored by hardware and is
- * treated as if it is One in EL2/EL3
+ * The ARMv8 translation table descriptor format defines AP[2:1] as the Access
+ * Permissions bits, and does not define an AP[0] bit.
+ *
+ * AP[1] is valid only for a stage 1 translation that supports two VA ranges
+ * (i.e. in the ARMv8A.0 architecture, that is the S-EL1&0 regime).
+ *
+ * AP[1] is RES0 for stage 1 translations that support only one VA range
+ * (e.g. EL3)
  */
-#define AP_RO				(0x1 << 5)
-#define AP_RW				(0x0 << 5)
+#define AP2_SHIFT			7
+#define AP2_RO				1
+#define AP2_RW				0
 
+#define AP1_SHIFT			6
+#define AP1_ACCESS			1
+#define AP1_NO_ACCESS			0
+/*
+ * The following definitions must all be passed to the LOWER_ATTRS() macro to
+ * get the right bitmask.
+ */
+#define AP_RO				(AP2_RO << 5)
+#define AP_RW				(AP2_RW << 5)
 #define NS				(0x1 << 3)
 #define ATTR_NON_CACHEABLE_INDEX	0x2
 #define ATTR_DEVICE_INDEX		0x1
 #define ATTR_IWBWA_OWBWA_NTR_INDEX	0x0
 #define LOWER_ATTRS(x)			(((x) & 0xfff) << 2)
+
 /* Normal Memory, Outer Write-Through non-transient, Inner Non-cacheable */
 #define ATTR_NON_CACHEABLE		(0x44)
 /* Device-nGnRE */
