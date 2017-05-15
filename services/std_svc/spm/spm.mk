0581cb145046aa6c178c74b96c81ed6828d1c9a5
Achin Gupta <achin.gupta@arm.com>
2017-05-15 16:22:31 +0100

    Add support for a Secure Partition Manager in BL31
    This patch adds a Secure Partition Manager (SPM) that implements a standard
    service to:
    
    1. Enable access to a secure partition from the normal world
    2. Setup of a secure partition and delegation of events to it in the secure
       world
    
    Change-Id: Ic9e7bf090eeb4e56ffd64e97d25ee8234bd1ea17
    Signed-off-by: Achin Gupta <achin.gupta@arm.com>

---
+++ services/std_svc/spm/spm.mk
@@ -0,0 +1,52 @@
+#
+# Copyright (c) 2017, ARM Limited and Contributors. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# Neither the name of ARM nor the names of its contributors may be used
+# to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+ifneq (${SPD},none)
+        $(error "Error: SPD and SPM are incompatible build options.")
+endif
+ifneq (${ARCH},aarch64)
+        $(error "Error: SPM is only supported on aarch64.")
+endif
+ifneq (${ARM_BL31_IN_DRAM}, 1)
+        $(error "Error: SPM is only supported when BL31 is in Secure DRAM.")
+endif
+
+# SPM sources
+
+
+SPM_SOURCES		:=	$(addprefix services/std_svc/spm/,	\
+				spm_main.c				\
+				${ARCH}/spm_helpers.S			\
+				secure_partition_setup.c		\
+				secure_partition_xlat_tables.c		\
+				${ARCH}/secure_partition_exceptions.S)
+
+# Let the top-level Makefile know that we intend to include a BL32 image
+NEED_BL32		:=	yes
