9de4752299778890774ca98799ecd986fdaee112
Achin Gupta <achin.gupta@arm.com>
2017-05-20 21:43:30 +0100

    SPM: Rename secure partition specifc data structures
    This patch renames variable names and updates comments to ensure that data
    structures that hold information specific to a secure partition use either
    "secure_partition_" or "sp_" terminology.
    
    Change-Id: Icd6ae62c8b55d031ac11e47676178f02ca5d9811
    Signed-off-by: Achin Gupta <achin.gupta@arm.com>

--- services/std_svc/spm/spm_private.h
+++ services/std_svc/spm/spm_private.h
@@ -56,31 +56,32 @@
  * Constants that allow assembler code to preserve callee-saved registers of the
  * C runtime context while performing a security state switch.
  ******************************************************************************/
-#define SPM_C_RT_CTX_X19		0x0
-#define SPM_C_RT_CTX_X20		0x8
-#define SPM_C_RT_CTX_X21		0x10
-#define SPM_C_RT_CTX_X22		0x18
-#define SPM_C_RT_CTX_X23		0x20
-#define SPM_C_RT_CTX_X24		0x28
-#define SPM_C_RT_CTX_X25		0x30
-#define SPM_C_RT_CTX_X26		0x38
-#define SPM_C_RT_CTX_X27		0x40
-#define SPM_C_RT_CTX_X28		0x48
-#define SPM_C_RT_CTX_X29		0x50
-#define SPM_C_RT_CTX_X30		0x58
-#define SPM_C_RT_CTX_SIZE		0x60
-#define SPM_C_RT_CTX_ENTRIES		(SPM_C_RT_CTX_SIZE >> DWORD_SHIFT)
+#define SP_C_RT_CTX_X19		0x0
+#define SP_C_RT_CTX_X20		0x8
+#define SP_C_RT_CTX_X21		0x10
+#define SP_C_RT_CTX_X22		0x18
+#define SP_C_RT_CTX_X23		0x20
+#define SP_C_RT_CTX_X24		0x28
+#define SP_C_RT_CTX_X25		0x30
+#define SP_C_RT_CTX_X26		0x38
+#define SP_C_RT_CTX_X27		0x40
+#define SP_C_RT_CTX_X28		0x48
+#define SP_C_RT_CTX_X29		0x50
+#define SP_C_RT_CTX_X30		0x58
+
+#define SP_C_RT_CTX_SIZE	0x60
+#define SP_C_RT_CTX_ENTRIES	(SP_C_RT_CTX_SIZE >> DWORD_SHIFT)
 
 
 #ifndef __ASSEMBLY__
 
 #include <stdint.h>
 
-typedef struct spm_context {
+typedef struct secure_partition_context {
 	uint64_t c_rt_ctx;
 	uint32_t flags;
 	cpu_context_t cpu_ctx;
-} spm_context_t;
+} secure_partition_context_t;
 
 uint64_t spm_secure_partition_enter(uint64_t *c_rt_ctx);
 void __dead2 spm_secure_partition_exit(uint64_t c_rt_ctx, uint64_t ret);
